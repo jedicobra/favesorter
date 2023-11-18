d6096dedad503bd83992a97e0e376a4312cd134c
max <jedicobra@gmail.com>
2023-11-18 00:23:07 -0800

    finished imdbulate script

--- src/scripts/imdbulate.py
+++ src/scripts/imdbulate.py
@@ -1,21 +1,32 @@
 from bs4 import BeautifulSoup
 from urllib import request
 import sys
-import re
+import shutil
 
 imdbUrl = "https://www.imdb.com/find/?q={query}"
 
 
 def getGame(searchQuery):
+
     url = imdbUrl.format(query=searchQuery)
     
     content = request.urlopen(request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
     soup = BeautifulSoup(content, features="html.parser")
-    imageUrl = soup.find("img", {"class": "ipc-image"}).get("src")
-    realUrl = "/".join(imageUrl.split("_V1_")[:-1]) + "_V1_.jpg"
+
+    firstResultImg = soup.find("img", {"class": "ipc-image"})
+    firstResultTitle = soup.find("a", {"class": "ipc-metadata-list-summary-item__t"}).contents[0]
     
-    print(realUrl)
+    lowResImageUrl = firstResultImg.get("src")
+    highResImageUrl = "/".join(lowResImageUrl.split("_V1_")[:-1]) + "_V1_.jpg"
+    
+    print(firstResultTitle)
+    print(highResImageUrl)
+
+    imgResponse, headers = request.urlretrieve(highResImageUrl, filename=firstResultTitle+".jpg")
 
 
 
-getGame( '+'.join(sys.argv[1:]) )
+if(len(sys.argv) == 1):
+    print("Enter a search query.\n")
+else:
+    getGame( '+'.join(sys.argv[1:]) )
