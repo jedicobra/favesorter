f80b3069a2d38ce5fb1d49956c9515e0b3b83705
max <jedicobra@gmail.com>
2023-11-21 00:36:30 -0800

    script updates

--- src/scripts/imdbulate.py
+++ src/scripts/imdbulate.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 from urllib import request
 import sys
 import shutil
+import re
+import os
 
 imdbUrl = "https://www.imdb.com/find/?q={query}"
+mobyGamesUrl = "https://www.mobygames.com/search/?q={query}"
+safeFilenameRegex = "[^A-Za-z0-9 -&+é]+"
 
-
-def getGame(searchQuery):
+def getImdb(searchQuery):
 
     url = imdbUrl.format(query=searchQuery)
     
@@ -18,15 +21,58 @@ def getGame(searchQuery):
     
     lowResImageUrl = firstResultImg.get("src")
     highResImageUrl = "/".join(lowResImageUrl.split("_V1_")[:-1]) + "_V1_.jpg"
+
+    safeTitle = re.sub(safeFilenameRegex, '', firstResultTitle)
+    print(safeTitle)
+
+    savedFilename = safeTitle+".jpg"
+    imgResponse, headers = request.urlretrieve(highResImageUrl, filename=savedFilename)
+    return savedFilename
+
+def getMoby(searchQuery):
+    url = mobyGamesUrl.format(query=searchQuery)
+
+    opener = request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    request.install_opener(opener)
     
-    print(firstResultTitle)
-    print(highResImageUrl)
+    content = request.urlopen(request.Request(url, headers={'User-Agent': 'Mozilla'})).read()
+    soup = BeautifulSoup(content, features="html.parser")
 
-    imgResponse, headers = request.urlretrieve(highResImageUrl, filename=firstResultTitle+".jpg")
+    firstResultUrl = soup.body.div.main.table.tr.td.a.get("href")
+    content = request.urlopen(request.Request(firstResultUrl, headers={'User-Agent': 'Mozilla'})).read()
+    soup = BeautifulSoup(content, features="html.parser")
+
+    imageUrl = soup.find("img", {"class": "img-box"}).get("src")
+    title = soup.find("h1", {"class": "mb-0"}).contents[0]
+    safeTitle = re.sub(safeFilenameRegex, '', title)
+    print(safeTitle)
+
+
+    savedFilename = filename=safeTitle + ".webp"
+    imgResponse, headers = request.urlretrieve(imageUrl, savedFilename)
+    return savedFilename
 
 
 
-if(len(sys.argv) == 1):
-    print("Enter a search query.\n")
-else:
-    getGame( '+'.join(sys.argv[1:]) )
+query = ''
+mode = "movie"
+
+while(query != 'q'):
+    query = input("> ").replace(' ', '+')
+    if query == "movie":
+        mode = "movie"
+        continue
+    elif query == "game":
+        mode = "game"
+        continue
+    elif query == "no":
+        os.remove(savedFilename)
+        continue
+
+    if mode == "movie":
+        savedFilename = getImdb(query)
+    elif mode == "game":
+        savedFilename = getMoby(query)
+
+print("Exiting.")
---
+++ src/scripts/updatejson.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+imagespath = "C:/users/jedic/source/repos/favesorter/public/images/"
+datapath = "C:/users/jedic/source/repos/favesorter/src/data.json"
+
+dataFile = open(datapath, 'r+')
+data = json.load(dataFile)
+needToUpdate = False
+
+for folder in os.listdir(imagespath):
+    # check that all folders have entries
+    if folder not in data:
+        needToUpdate = True
+        print("adding " + folder + " to data.json")
+        data[folder] = {}
+        data[folder]["foldername"] = folder
+        data[folder]["filenames"] = []
+
+    # make sure all the filenames are there
+    for file in os.listdir(imagespath + folder):
+        if file not in data[folder]["filenames"]:
+            needToUpdate = True
+            data[folder]["filenames"].append(file)
+
+dataFile.close()
+
+if needToUpdate:
+    dataFile = open(datapath, 'w+')
+    json.dump(data, dataFile)
+    dataFile.close()
